7229c36f6421506d8fb9113f2301a9cd0073ad9c
cynpardede <Cynthiapards@gmail.com>
2024-11-27 01:05:10 +0700

    Add files via upload

---
+++ TugasP10/src/TugasP10/Mahasiswa.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasP10;
+
+/**
+ *
+ * @author Cynthia
+ */
+public class Mahasiswa {
+    private String NIM, Nama, Alamat, MKul;
+    private String nilaiAkhir;
+ public Mahasiswa(String NIM, String Nama, String Alamat, String MKul, String nilaiAkhir){
+        this.NIM = NIM;
+        this.Nama = Nama;
+        this.Alamat = Alamat;
+        this.MKul = MKul;
+        this.nilaiAkhir = nilaiAkhir; 
+    }
+
+    public String getAlamat(){
+        return Alamat;
+    }
+
+    public String getNIM(){
+        return NIM;
+    }
+
+    public String getNama(){
+        return Nama;
+    }
+    
+    public String getMk(){
+        return MKul;
+    }
+    
+    public String getNilai(){
+        return nilaiAkhir;
+    }
+}
